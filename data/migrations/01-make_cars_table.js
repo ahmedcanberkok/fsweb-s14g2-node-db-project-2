c98dedf4414466b9fda73e56d328c43075caf805
Canberk Ok <canberk.ok@gmail.com>
2023-06-21 13:57:01 +0300

    migrations file add

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // SİHRİNİZİ GÖSTERİN
+  return knex.schema.createTable("cars", t=> {
+    t.increments("cars_id");
+    t.string("vin").notNullable().unique()
+    t.string("make").notNullable()
+    t.string("model").notNullable()
+    t.integer("mileage").notNullable()
+    t.string("title")
+    t.string("transmission")
+  })
 };
 
 exports.down = function (knex) {
   // SİHRİNİZİ GÖSTERİN
+  return knex.schema.dropTableIfExists("cars");
 };
