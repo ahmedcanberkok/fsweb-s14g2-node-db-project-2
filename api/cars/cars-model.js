c8575147ac3ad859885ccdb4223f0d3aabf25f75
Canberk Ok <canberk.ok@gmail.com>
2023-06-21 14:58:15 +0300

    cars models finish

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,24 @@
+const db = require("../../data/db-config")
+
 const getAll = () => {
   // HOKUS POKUS
+  return db("cars");
 }
 
-const getById = () => {
+const getById = (carId) => {
   // HOKUS POKUS
+  return db("cars").where("id",carId).first()
 }
 
-const create = () => {
+const create = async (carEntity) => {
   // HOKUS POKUS
+  const [id] = db("cars").insert(carEntity);
+  return getById(id);
+
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,44 @@
 // ESNEK
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+    const defaultCars = [
+        {
+            vin: "123",
+            make: "Toyata",
+            model: "Corolla",
+            mileage: 1234
+        },
+        {
+            vin: "1234",
+            make: "Audi",
+            model: "A6",
+            mileage: 1234
+        },
+        {
+            vin: "12345",
+            make: "volkswagen",
+            model: "Polo",
+            mileage: 1234
+        },
+        {
+            vin: "123456",
+            make: "Seat",
+            model: "LeonFR",
+            mileage: 1234
+        },
+        {
+            vin: "1234567",
+            make: "Renault",
+            model: "Clio",
+            mileage: 1234
+        },
+    ];
+
+exports.seed = async function(knex) {
+    // Deletes ALL existing entries
+    await knex('cars').truncate()
+    await knex('cars').insert(defaultCars);
+  };
+  
